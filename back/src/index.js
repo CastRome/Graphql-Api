2e881dd4243589501f80c3cccb540bda725b410d
Ruben Dario Castilla Romero <67343176+CastRome@users.noreply.github.com>
2022-12-14 21:29:26 -0500

    minor fix backend

--- back/src/index.js
+++ back/src/index.js
@@ -9,10 +9,7 @@ const typeDefs = `#graphql
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
   # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
+
   enum CharacterStatus{
     Alive
     Dead
@@ -31,22 +28,10 @@ const typeDefs = `#graphql
   # clients can execute, along with the return type for each. In this
   # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
-    books: [Book]
     character(id:ID):Character
     characters:[Character]
   }
 `;
-const books = [
-  {
-    title: 'The Awakening',
-    author: 'Kate Chopin',
-  },
-
-  {
-    title: 'City of Glass',
-    author: 'Paul Auster',
-  },
-];
 
 // Resolvers define how to fetch the types defined in your schema.
 
@@ -54,7 +39,6 @@ const books = [
 
 const resolvers = {
   Query: {
-    books: () => books,
     characters: () => fetchCharacters(),
     character: (parents, args) => {
       const { id } = args;
@@ -63,25 +47,6 @@ const resolvers = {
     },
   },
 };
-
-function fetchEpisodes() {
-  // More info about the fetch function? https://github.com/bitinn/node-fetch#json
-  return fetch('https://rickandmortyapi.com/api/episode/')
-    .then((res) => res.json())
-    .then((json) => json.results);
-}
-
-function fetchEpisodeById(id) {
-  return fetch('https://rickandmortyapi.com/api/episode/' + id)
-    .then((res) => res.json())
-    .then((json) => json);
-}
-
-function fetchEpisodeByUrl(url) {
-  return fetch(url)
-    .then((res) => res.json())
-    .then((json) => json);
-}
 
 function fetchCharacters() {
   return fetch('https://rickandmortyapi.com/api/character/')
@@ -95,12 +60,6 @@ function fetchCharacterById({ id }) {
     .then((json) => json);
 }
 
-function fetchCharacterByUrl(url) {
-  // More info about the fetch function? https://github.com/bitinn/node-fetch#json
-  return fetch(url)
-    .then((res) => res.json())
-    .then((json) => json);
-}
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
 const server = new ApolloServer({
@@ -109,9 +68,6 @@ const server = new ApolloServer({
 });
 
 // Passing an ApolloServer instance to the `startStandaloneServer` function:
-//  1. creates an Express app
-//  2. installs your ApolloServer instance as middleware
-//  3. prepares your app to handle incoming requests
 const listenServer = async () => {
   const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 },
@@ -119,6 +75,3 @@ const listenServer = async () => {
   console.log(`🚀  Server ready at: ${url}`);
 };
 listenServer();
-// server.listen().then(({ url }) => {
-//   console.log(`🚀  Server ready at ${url}`);
-// });
